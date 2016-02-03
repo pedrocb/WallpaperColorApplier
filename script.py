e432da6d2d7e1cefac63396f909e7ef0fe4ed8bd
pedrocb <pedrocb_c-s@hotmail.com>
2016-02-03 07:08:21 +0000

    added command line arguments (nothing fancy yet)

--- script.py
+++ script.py
@@ -2,20 +2,15 @@ import png
 import random
 import colorsys
 import sys
+import argparse
 
-imageToModify = "1941277.png"
-newImageName = "newWallpaper1.png"
-
-
-colorHue = random.random()*360.0;
-colorSaturation = random.random()*50.0 + 50.0;
 colorValueRangeMin = 0.10;
 
 def apply_color():
-    image = open(imageToModify)
+    image = open(args.original_file)
     r=png.Reader(file=image)
     imageArray = list(r.read()[2])
-    newImage = open(newImageName,"wb")
+    newImage = open(args.new_file,"wb")
     w=png.Writer(len(imageArray[0]),len(imageArray))
     newArray = list()
     width = len(imageArray[0])
@@ -24,13 +19,22 @@ def apply_color():
         for x in range(width):
             color = imageArray[y][x]
             hsv = colorsys.rgb_to_hsv(color/255.0,color/255.0,color/255.0)
-            rgb = colorsys.hsv_to_rgb(colorHue/360.0,colorSaturation/100.0, hsv[2]*(1-colorValueRangeMin) + colorValueRangeMin)
+            rgb = colorsys.hsv_to_rgb(args.hue[0]/360.0,args.saturation[0]/100.0, hsv[2]*(1-colorValueRangeMin) + colorValueRangeMin)
             for i in range(3):
                 newArray[y].append(rgb[i]*255)
     png.from_array(newArray,'RGB').save(newImage)
     return imageArray
     
 if "__main__" == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('hue', metavar='H', type=int, nargs='+',
+                        help='hue of the color')
+    parser.add_argument('saturation', metavar='S', type=int, nargs='+', help='saturation of the color')
+    parser.add_argument('original_file', metavar='i', help='File to modify')
+    parser.add_argument('new_file', metavar='o', help="New File")
+    args = parser.parse_args()
+    print args.hue
+    print args.saturation
     apply_color()
     
     
